3f2d290ecd92a4dc80fa34949fb4ee88d3a7ae50
sudharsan24-s <sudharsan242002@gmail.com>
2023-08-23 21:53:03 +0530

    changed

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 8080;
-
 const baseUrl = '/calculator'
-
 app.use(express.json());
-
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
+    return res.send('Hello World!');
     return res.send('');
 });
 
 baseRouter.post('/add', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
     res.json({ "": null });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first - second });
     res.json({ "": null });
 });
 
