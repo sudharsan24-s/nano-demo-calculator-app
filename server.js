b3aff3109896a5a4f489db5cd92b95ffc14d3557
sudharsan24-s <sudharsan242002@gmail.com>
2023-08-23 21:55:58 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
-const baseUrl = '/calculator'
+const baseUrl = "/calculator"
 app.use(express.json());
 const baseRouter = express.Router();
 
